3eaf125368987977a5e0aed2e2427b7ae1f7a70c
khyreerusydi <nicoraventbh@gmail.com>
2019-05-03 21:31:18 +0800

    created scaffold for Sessions and master_sessions table

---
+++ db/migrate/20190503100119_master_sessions.rb
@@ -0,0 +1,10 @@
+class MasterSessions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :master_sessions do |t|
+      t.string :session_name
+      t.string :session_password
+      t.string :proposed_date
+      t.timestamps
+    end
+  end
+end
