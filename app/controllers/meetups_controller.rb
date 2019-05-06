851ca5f381d655ee3b156a8b52bd71a23708d2d8
khyreerusydi <nicoraventbh@gmail.com>
2019-05-06 14:39:56 +0800

    created new nested form

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -69,6 +69,6 @@ class MeetupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def meetup_params
-      params.require(:meetup).permit(:name, :description, :password, schedule_attributes: [:id, :date, :_destroy])
+      params.require(:meetup).permit(:name, :description, :password, schedules_attributes: [:id, :date, :_destroy])
     end
 end
