8c434e45f5ec93f866abc35dfc5d2e82aed074ab
Chang <ainurlindale@gmail.com>
2019-05-06 09:46:39 +0800

    front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'schedules#index'
+
+  get '/create' => 'schedules#create', as: 'create_schedule'
+  get '/login'  => 'schedules#login',  as: 'login_schedule'
 end
