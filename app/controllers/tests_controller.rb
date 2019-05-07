4babb5ef67d5d24836a869674cad1f83dbe3e3d2
Chang <ainurlindale@gmail.com>
2019-05-07 17:08:31 +0800

    front end login now loads from db

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,7 +4,28 @@ class TestsController < ApplicationController
 
   end
 
+  def show
+    # render plain:
+    # redirect_to '/meetups/@post'
+  end
+
   def create
+  value = params[:test]["session"]
+
+
+  meetups = Meetup.all
+
+  meetups.each do |meetup|
+    if meetup.id == value.to_i || meetup.name == value
+        # render plain: params[:test]
+        route_id = meetup.id
+        redirect_to "/meetups/#{route_id}"
+    end
+  end
+
+  end
+
+  def new
 
   end
 
