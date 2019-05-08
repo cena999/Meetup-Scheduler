fdbb4101680ef3eea4dd43e992eacb3f8dccebea
Chang <ainurlindale@gmail.com>
2019-05-08 22:26:29 +0800

    added seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Meetup.create(name: 'Teo', description: 'Teo')
+Meetup.create(name: 'Chang', description: 'Chang')
+Meetup.create(name: 'Hao', description: 'Hao')
+
+Schedule.create(date: '05/25/2019' , meetup_id: 1)
+Schedule.create(date: '04/18/2019' , meetup_id: 1)
+Schedule.create(date: '03/16/2019', meetup_id: 1)
+
+Schedule.create(date: '11/23/2019', meetup_id: 2)
+Schedule.create(date: '07/25/2019', meetup_id: 2)
+Schedule.create(date: '09/09/2019', meetup_id: 2)
+
+Schedule.create(date: '01/14/2019', meetup_id: 3)
+Schedule.create(date: '08/18/2019', meetup_id: 3)
+Schedule.create(date: '04/28/2019', meetup_id: 3)
+
+puts 'DATA SEEDEDDED'
